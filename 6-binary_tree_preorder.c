28264374a73abe8161f6a3d2313f9acccef8d2e6
Zarina138 <zrinaabdulazizova@gmail.com>
2025-10-03 15:31:42 +0000

    Task

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-
+#include <stdlib.h>
 /**
  * binary_tree_preorder - Goes through a binary tree using pre-order traversal
  * @tree: Pointer to the root node of the tree to traverse
